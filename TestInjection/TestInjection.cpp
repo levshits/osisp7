c9fa11be2e5970c6bb6fb116cdfda41c13d12039
levshits <levshits.l@gmail.com>
2014-12-09 10:24:29 +0300

    Ready

---
+++ TestInjection/TestInjection.cpp
@@ -0,0 +1,20 @@
+// TestInjection.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "windows.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	system("pause");
+	MessageBox(NULL, L"MessageBox Text", L"MessageBox caption", MB_OK);
+	system("pause");
+	MessageBox(NULL, L"MessageBox Text", L"MessageBox caption", MB_OK);
+	system("pause");
+	MessageBox(NULL, L"MessageBox Text", L"MessageBox caption", MB_OK);
+	system("pause");
+	MessageBox(NULL, L"MessageBox Text", L"MessageBox caption", MB_OK);
+	system("pause");
+	return 0;
+}
+
